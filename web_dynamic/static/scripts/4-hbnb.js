381b6f3151e5bb4cac7c63fb6d214ee6829ba0dd
Muna Saeed <munasaeed6367@gmail.com>
2024-03-03 12:54:34 -0800

    5. Filter places by Amenity

--- web_dynamic/static/scripts/4-hbnb.js
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -71,9 +71,45 @@ $(document).ready(function() {
 		});
 	}
 
+	// Function to handle search button click
+	function handleSearchButtonClick() {
+		// Make a new POST request to places_search with the list of checked amenities
+		$.ajax({
+			type: 'POST',
+			url: 'http://0.0.0.0:5001/api/v1/places_search/',
+			contentType: 'application/json',
+			data: JSON.stringify({ amenities: Object.keys(checkedAmenities) }),
+			success: function(response) {
+				// Clear existing places
+				$('.places').empty();
+
+				// Loop through the result and create article tags
+				for (var i = 0; i < response.length; i++) {
+					var place = response[i];
+					var articleHTML = '<article><div class="title_box"><h2>' +
+						place.name + '</h2><div class="price_by_night">$' +
+						place.price_by_night + '</div></div><div class="information">' +
+						'<div class="max_guest">' + place.max_guest + ' Guest' +
+						(place.max_guest !== 1 ? 's' : '') + '</div><div class="number_rooms">' +
+						place.number_rooms + ' Bedroom' + (place.number_rooms !== 1 ? 's' : '') +
+						'</div><div class="number_bathrooms">' + place.number_bathrooms +
+						' Bathroom' + (place.number_bathrooms !== 1 ? 's' or '') + '</div></div>' +
+						'<div class="description">' + place.description + '</div></article>';
+					$('.places').append(articleHTML);
+				}
+			},
+			error: function(error) {
+				console.error('Error fetching places:', error);
+			}
+		});
+	}
+
 	// Call the function to update API status and places on page load
 	updatePlacesSearch();
 	updateApiStatus();
+
+	// Attach the click event handler to the search button
+	$('button').on('click', handleSearchButtonClick);
 
 
 	$('.popover input[type="checkbox"]').on('change', function() {
